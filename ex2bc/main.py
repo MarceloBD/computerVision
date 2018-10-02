c776671ce3e3f6f75241611d8c73d9eb9c6f08b2
MarceloBD <marcelodianib@gmail.com>
2018-10-02 19:54:52 -0300

    done

--- ex2bc/main.py
+++ ex2bc/main.py
@@ -1,9 +1,9 @@
-from projecao import projecao
+from projecao import Projecao
 
 
 if __name__ == '__main__':
 
-	Tc = [[-1000], [1000],[5000]]
+	Tc = [-1000 ,1000 ,5000]
 	roty = 160
 	f = 16
 	sx, sy = [0.01,0.01]
@@ -13,3 +13,13 @@ if __name__ == '__main__':
 	Pw = [[1000,1000,1500,1500], 
 		  [1000,1500,1500,1000], 
 		  [ 500,500, 500, 500]]
+
+
+	proj = Projecao()
+	h = proj.homogenea(0, roty, 0, Tc[0], Tc[1], Tc[2])
+	Pc = proj.mundo_para_camera(Pw,h)
+	pj = proj.proj_perspectiva_mm(Pc, f)
+
+	pjp = proj.proj_perspectiva_pixel(pj, sx, sy, ox, oy)
+
+	proj.printMatrix(pjp)
--- ex2bc/projecao.py
+++ ex2bc/projecao.py
@@ -11,9 +11,9 @@ class Projecao:
 	# @f distancia focal da camera [mm]
 	# retorna projecao do ponto P [mm] 
 		p = copy(P)
-		for i in xrange(len(P[0])):
-			p[0][i] = f*P[0][i]/P[2][i]
-			p[1][i] = f*P[1][i]/P[2][i]
+		for i in range(len(P[0])):
+			p[0][i] = f*P[0][i]/float(P[2][i])
+			p[1][i] = f*P[1][i]/float(P[2][i])
 			p[2][i] = f
 		return p
 
@@ -24,50 +24,52 @@ class Projecao:
 	# @ox coordenada de projecao do eixo optico [pixel]
 	# retorna coordenada do pixel [pixel]
 		pixel = np.zeros(shape=(2, len(p[0])))
-		for i in xrange(len(p[0])):
+		for i in range(len(p[0])):
 			pixel[0][i] = -int(p[0][i])/sx + ox 
 			pixel[1][i] = -int(p[1][i])/sy + oy
 		return pixel
 
 	def printMatrix(self, matrix):
-		for i in xrange(len(matrix)):
-			for j in xrange(len(matrix[0])):
-				print matrix[i][j],
-			print			
+		for i in range(len(matrix)):
+			for j in range(len(matrix[0])):
+				print (matrix[i][j], end=" ")
+			print("")			
 
 	###########################################################
 
 
-	def mundo_para_camera(Pw,H):
+	def mundo_para_camera(self,Pw,H):
 	# Pw matriz 3xN [mm] coordenada global
 	# H matriz de transformacao homogenea [mm]
 	# retorna ponto em coordenada da camera 3xN[mm]
-	P = copy(Pw)
-	Pc = copy(Pw)
-	P = vstack(Pw,[1 for _ in range(Pw[0])])
+		
+		P = copy(Pw)
+		Pc = copy(Pw)
+		P = np.vstack((Pw,[1 for _ in range(len(Pw[0]))]))
 		for n in range(len(P[0])):
-			x,y,z = [0,0,0]
+			Pc[0][n], Pc[1][n], Pc[2][n] = [0,0,0]
 			for i in range(4):
 				Pc[0][n] += H[0][i]*P[i][n]
 				Pc[1][n] += H[1][i]*P[i][n]
 				Pc[2][n] += H[2][i]*P[i][n]
 		return Pc
 
-	def homogenea(rotx, roty, rotz, dx, dy, dz):
+	def homogenea(self,rotx, roty, rotz, dx, dy, dz):
 	# rotx, roty, rotz rotacao em graus 
 	# dx, dy, dz deslocamento em [mm]
 	# retorna matriz de transformacao homogenea (3xN)
+		pi = np.pi
 		rotxR = rotx*pi/180.0  
 		rotyR = roty*pi/180.0 
 		rotzR = rotz*pi/180.0  
-		cz = cos(rotzR) 
-		sz = sin(rotzR)
-		cy = cos(rotyR) 
-		sy = sin(rotyR)
-		cx = cos(rotxR) 
-		sx = sin(rotxR)
+		cz = np.cos(rotzR) 
+		sz = np.sin(rotzR)
+		cy = np.cos(rotyR) 
+		sy = np.sin(rotyR)
+		cx = np.cos(rotxR) 
+		sx = np.sin(rotxR)
 
-		H = [[cz*cy, -sz*cx+cz*sy*sx, sz*sx+cz*sy*cx,  -dx],
-			 [sz*cy, cz*cx+sz*sy*sx,  -cz*sx+sz*sy*cx, -dy],
-			 [-sy,   cy*sx,           cx*cy,           -dy]]
+		H = [[cz*cy, -1*sz*cx+cz*sy*sx, sz*sx+cz*sy*cx,  -1*dx],
+			 [sz*cy, cz*cx+sz*sy*sx,  -cz*sx+sz*sy*cx, -1*dy],
+			 [-1*sy,   cy*sx,           cx*cy,           -1*dy]]
 		return H
